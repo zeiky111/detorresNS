19637db6096dc637d65af2dce4afe84090182cd8
zeiky111 <148170998+zeiky111@users.noreply.github.com>
2024-05-23 16:45:26 +0800

    Add files via upload

--- javascript/js/js.js
+++ javascript/js/js.js
@@ -2,29 +2,30 @@ function addition(){
     let x=0;
     x= document.getElementById("input1").value;
     if(x==false){
-        alert("box cannot be empty");
+        alert("box 1 cannot be empty");
         alert("enter a number");
     }
     let y=0;
     y= document.getElementById("input2").value;
     if(y==false){
-        alert("box cannot be empty");
+        alert("box 2 cannot be empty");
         alert("enter a number");
     }
     ans1= Number(x)+Number(y);
+    
     document.getElementById("sum").value=ans1;
 }
 function subtraction(){
     let n=0;
     n= document.getElementById("input3").value;
     if(n==false){
-        alert("box cannot be empty");
+        alert("box 1 cannot be empty");
         alert("enter a number");
     }
     let a=0;
     a= document.getElementById("input4").value;
     if(a==false){
-        alert("box cannot be empty");
+        alert("box 2 cannot be empty");
         alert("enter a number");
     }
     ans2= Number(n)-Number(a);
@@ -34,13 +35,13 @@ function multiplication(){
     let c=0;
     c= document.getElementById("input5").value;
     if(c==false){
-        alert("box cannot be empty");
+        alert("box 1 cannot be empty");
         alert("enter a number");
     }
     let d=0;
     d= document.getElementById("input6").value;
     if(d==false){
-        alert("box cannot be empty");
+        alert("box 2 cannot be empty");
         alert("enter a number");
     }
     ans3= Number(c)*Number(d);
@@ -50,13 +51,13 @@ function division(){
     let f=0;
     f= document.getElementById("input7").value;
     if(f==false){
-        alert("box cannot be empty");
+        alert("box 1 cannot be empty");
         alert("enter a number");
     }
     let g=0;
     g= document.getElementById("input8").value;
     if(g==false){
-        alert("box cannot be empty");
+        alert("box 2 cannot be empty");
         alert("enter a number");
     }
     ans4= Number(f)/Number(g);
